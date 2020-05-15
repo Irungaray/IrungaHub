23aedbb78a86ba518631b42ebdda53e637342c9d
Irungaray <LionelIrungaray@GMail.com>
2020-05-15 14:22:18 -0300

    Añadido Carousel, Categories y Footer

---
+++ src/components/Categories.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'
+import '../assets/styles/components/Categories.scss'
+
+const Categories = ({ children }) => (
+    <div className="categories">
+        <h2 className="categories__title">Añadidas recientemente</h2>
+        {children}
+    </div>
+)
+
+export default Categories
