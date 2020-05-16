c26344739bbe81778443e620124db23dc7223bbf
Irungaray <LionelIrungaray@GMail.com>
2020-05-15 21:08:37 -0300

    Añadida y configurada FakeAPI

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -7,40 +7,52 @@ import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
-        <Categories>
-            <Carousel title="Mi lista">
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const App = () => {
+    const [ videos, setVideos ] = useState([])
 
-        <Categories title="Populares">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    useEffect (() => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, [])
 
-        <Categories title="Originales de IrungaHub">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    console.log(videos)
 
-        <Footer />
-    </div>
-)
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            <Categories>
+                <Carousel title="Mi lista">
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Categories title="Populares">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales de IrungaHub">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    )
+}
 
 export default App
